8f865c77f137dd2b767d00ebb1b98d7bbbf97915
Dmytro Suvorov <suslamo@gmail.com>
2015-07-20 20:10:48 +0300

    add show all collapsed on sidebar click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,15 @@
 // jshint devel:true
 
 $('body').scrollspy({
-    target: '.bs-docs-sidebar',
+    target: '.validation-trace .sidebar',
     offset: 40
+});
+
+$(".validation-trace .sidebar a").click(function(e) {
+  var links = $(this).parents('.validation-trace .sidebar li').children('a');
+  $(links.get().reverse()).each(function() {
+     $($(this).attr("data-target-id")).collapse('show');
+  });
 });
 
 function renderData(id, json) {
