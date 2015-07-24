4a04e9303b154c2b449ff9f306cfe133c0713761
Dmytro Suvorov <suslamo@gmail.com>
2015-07-24 16:16:03 +0300

    ported to bootstrap#2 and jquery#1.9.1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,19 @@
 // jshint devel:true
 
-$('body').scrollspy({
-    target: '.validation-trace .sidebar',
-    offset: 40
-});
+$(function() {
+    /*
+    $('body').scrollspy({
+        target: '.validation-trace .sidebar',
+        offset: 40
+    });
+    */
 
-$(".validation-trace .sidebar a").click(function() {
-  var links = $(this).parents('.validation-trace .sidebar li').children('a');
-  $(links.get().reverse()).each(function() {
-     $($(this).attr("data-target-id")).collapse('show');
-  });
+    $(".validation-trace .sidebar a").click(function() {
+      var links = $(this).parents('.validation-trace .sidebar li').children('a');
+      $(links.get().reverse()).each(function() {
+         $($(this).attr("data-target-id")).collapse('show');
+      });
+    });
 });
 
 function renderCustomer(id, el, json) {
